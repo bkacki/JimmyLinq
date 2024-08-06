27ae31b1e46e0557335c59523744187ab6923760
Bartek <bartlomiej.kacki@gmail.com>
2024-08-06 21:18:04 +0200

    Added test method

--- JimmyLinqUnitTests/ComicAnalyzerTests.cs
+++ JimmyLinqUnitTests/ComicAnalyzerTests.cs
@@ -29,5 +29,28 @@ namespace JimmyLinqUnitTests
             Assert.AreEqual(2, groups.First().First().Issue);
             Assert.AreEqual("Numer 2", groups.First().First().Name);
         }
+
+        [TestMethod]
+        public void ComicAnalyzer_Should_Generate_A_List_Of_Reviews()
+        {
+            var testReviews = new[]
+            {
+                new Review() { Issue = 1, Critic = Critics.MuddyCritic, Score = 14.5 },
+                new Review() { Issue = 1, Critic = Critics.RottenTornadoes, Score = 59.93 },
+                new Review() { Issue = 2, Critic = Critics.MuddyCritic, Score = 40.3 },
+                new Review() { Issue = 2, Critic = Critics.RottenTornadoes, Score = 95.11 },
+            };
+
+            var expectedResult = new[]
+            {
+                "MuddyCritic oceni³ nr 1 'Numer 1' na 14,5",
+                "RottenTornadoes oceni³ nr 1 'Numer 1' na 59,93",
+                "MuddyCritic oceni³ nr 2 'Numer 2' na 40,3",
+                "RottenTornadoes oceni³ nr 2 'Numer 2' na 95,11",
+            };
+
+            var actualResults = ComicAnalyzer.GetReviews(testComics, testReviews).ToList();
+            CollectionAssert.AreEqual(expectedResult, actualResults);
+        }
     }
 }
